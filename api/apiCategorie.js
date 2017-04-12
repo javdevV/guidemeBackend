3382df1295dd95bfd52026d3d7974aa5b7e1ad75
Houssem Missaoui <houssem.missaoui@esprit.tn>
2017-04-12 11:36:36 +0100

    before merge

--- api/apiCategorie.js
+++ api/apiCategorie.js
@@ -7,11 +7,16 @@ var request = require('request');
 var Category = require('../models/categories');
 
 router.get('/categories',function (req,res) {
-    var url = "https://api.paris.fr/api/data/1.0/Equipements/get_categories/?token=fb1cba01bf7191ab033e7d7a441667ef18f77f14581e00b3b577871083fbfed4";
-    request.get(url,function(error, response, body){
-        var l= JSON.parse(body);
-        res.json(l.data);
-    });
+    // var url = "https://api.paris.fr/api/data/1.0/Equipements/get_categories/?token=fb1cba01bf7191ab033e7d7a441667ef18f77f14581e00b3b577871083fbfed4";
+    // request.get(url,function(error, response, body){
+    //     var l= JSON.parse(body);
+    //     res.json(l.data);
+    // });
+    Category.find(function (err, cat) {
+        if (err)
+            res.json(err);
+        res.json(cat);
+    })
 });
 router.get('/syncCategories', function (req, res) {
      var url = "https://api.paris.fr/api/data/1.0/Equipements/get_categories/?token=fb1cba01bf7191ab033e7d7a441667ef18f77f14581e00b3b577871083fbfed4";
