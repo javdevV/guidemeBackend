17f1122d801c87e04f273e6cc1f9c40adda6e036
Sourour Benzarti <sourour.benzarti@esprit.tn>
2017-04-18 22:07:58 +0100

    adding https

--- app.js
+++ app.js
@@ -3,6 +3,15 @@
  */
 
 'use strict';
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var privateKey  = fs.readFileSync('server.key', 'utf8');
+var certificate = fs.readFileSync('server.cert', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate};
+
+
 //
 // Set default node environment to development
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
@@ -23,13 +32,21 @@ if (config.seedDB) {
 
 // Setup server
 var app = express();
+
+
+
 var server = require('http').createServer(app);
+// your express configuration here --sourour
+//var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+httpsServer.listen(8443);
+
 
 require('./config/express')(app);
 require('./routes')(app);
 
 // Start server var port = normalizePort(process.env.PORT || '3000');
-
+//httpServer.listen(8080); --sourour
 server.listen(config.port, config.ip, function() {
   console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
 });
